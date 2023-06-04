32081e61332c4b9e0936d4be44675403e68ae09e
SHekhvatovAlexander <Alexander.shekhvatov@yandex.com>
2023-06-04 14:26:24 +0700

    Добавил выполненное задание 2

---
+++ homework/task_1.2.py
@@ -0,0 +1,81 @@
+# Задача 1.2.
+
+# Пункт A. 
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+
+import random
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+random_songs = random.sample(my_favorite_songs, k=3)
+
+total_time = sum([song[1] for song in random_songs])
+
+print(f"Три песни звучат {total_time:.0f} минут")
+
+# Пункт B. 
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+import datetime
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+random_songs = random.sample(list(my_favorite_songs_dict.keys()), 3)
+total_time = sum([my_favorite_songs_dict[song] for song in random_songs])
+
+minutes = int(total_time)
+seconds = int((total_time - minutes) * 60)
+
+print(f"Три песни звучат {minutes} минут {seconds} секунд.")
+
+
+
+# Дополнительно для пунктов A и B
+# Пункт C.
+# Сгенерируйте случайные песни с помощью модуля random
+# import random
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+random_songs = random.sample(list(my_favorite_songs_dict.keys()), 3)
+
+print(f"Randm songs: {random_songs}")
+
+# Дополнительно 
+# Пункт D.
+# Переведите минуты и секунды в формат времени. Используйте модуль datetime 
